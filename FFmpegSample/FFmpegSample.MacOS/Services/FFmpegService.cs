6faf028695ba9d3c5d1811751a24ff13504cb59e
Nacompllo <lilloserg96@outlook.com>
2021-10-18 15:40:34 +0200

    Added FfmpegService in macOS

---
+++ FFmpegSample/FFmpegSample.MacOS/Services/FFmpegService.cs
@@ -0,0 +1,27 @@
+﻿using FFmpegSample.Services;
+using Foundation;
+using System;
+
+namespace FFmpegSample.MacOS.Services
+{
+    public class FFmpegService : IFFmpegService
+    {
+        public void ExecuteFFmpeg()
+        {
+            try
+            {
+                var launchPath = NSBundle.MainBundle.PathForResource("ffmpeg", ofType: "");
+                var compressTask = new NSTask();
+                compressTask.LaunchPath = launchPath;
+                compressTask.Arguments = new string[] { "ffmpeg -i downloads/tyson.mp4 -c copy -an onlyaudio.mp4" };
+                compressTask.StandardInput = NSFileHandle.FromNullDevice();
+                compressTask.Launch();
+                compressTask.WaitUntilExit();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
--- FFmpegSample/FFmpegSample/Services/IFFmpegService.cs
+++ FFmpegSample/FFmpegSample/Services/IFFmpegService.cs
@@ -4,6 +4,6 @@ namespace FFmpegSample.Services
 {
     public interface IFFmpegService
     {
-        Task ExecuteFFmpeg(string parameter);
+        void ExecuteFFmpeg();
     }
 }

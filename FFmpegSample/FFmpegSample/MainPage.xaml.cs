31ba7d65a059fe0091fbb0af6a78954459bec3d9
Nacompllo <lilloserg96@outlook.com>
2021-10-18 15:42:38 +0200

    Added call to FFmpeg service

--- FFmpegSample/FFmpegSample/MainPage.xaml.cs
+++ FFmpegSample/FFmpegSample/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FFmpegSample.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,17 @@ namespace FFmpegSample
 {
     public partial class MainPage : ContentPage
     {
+        private readonly IFFmpegService _fFmpegService;
+
         public MainPage()
         {
             InitializeComponent();
+            _fFmpegService = DependencyService.Get<IFFmpegService>();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _fFmpegService.ExecuteFFmpeg();
         }
     }
 }
